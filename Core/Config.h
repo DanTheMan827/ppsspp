f778351ba3c8f20e562c51c9909d68173d55a5c5
Daniel <790119+DanTheMan827@users.noreply.github.com>
2023-09-05 20:13:25 -0500

    Update Config.h

--- Core/Config.h
+++ Core/Config.h
@@ -182,7 +182,7 @@ public:
 	bool bFullScreen;
 	bool bFullScreenMulti;
 	int iForceFullScreen = -1; // -1 = nope, 0 = force off, 1 = force on (not saved.)
-	int iInternalResolution;  // 0 = Auto (native), 1 = 1x (480x272), 2 = 2x, 3 = 3x, 4 = 4x and so on.
+	float iInternalResolution;  // 0 = Auto (native), 1 = 1x (480x272), 2 = 2x, 3 = 3x, 4 = 4x and so on.
 	int iAnisotropyLevel;  // 0 - 5, powers of 2: 0 = 1x = no aniso
 	int iMultiSampleLevel;
 	int bHighQualityDepth;
